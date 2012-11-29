a1605554b82775c395bcc40725c373e092caed85
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 13:11:59 +0100

    Make C examples Protocol 2.0 compatible

--- software/examples/c/example_simple.c
+++ software/examples/c/example_simple.c
@@ -9,23 +9,20 @@
 #define UID "xyz" // Change to your UID
 
 int main() {
-	// Create IP connection to brickd
+	// Create IP connection
 	IPConnection ipcon;
-	if(ipcon_create(&ipcon, HOST, PORT) < 0) {
-		fprintf(stderr, "Could not create connection\n");
-		exit(1);
-	}
+	ipcon_create(&ipcon);
 
 	// Create device object
 	IndustrialQuadRelay iqr;
-	industrial_quad_relay_create(&iqr, UID); 
+	industrial_quad_relay_create(&iqr, UID, &ipcon); 
 
-	// Add device to IP connection
-	if(ipcon_add_device(&ipcon, &iqr) < 0) {
-		fprintf(stderr, "Could not connect to Bricklet\n");
+	// Connect to brickd
+	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
+		fprintf(stderr, "Could not connect\n");
 		exit(1);
 	}
-	// Don't use device before it is added to a connection
+	// Don't use device before ipcon is connected
 
 	// Turn relay 0, 3 on and relay 1, 2 off.
 	industrial_quad_relay_set_value(&iqr, 1 | 8);
