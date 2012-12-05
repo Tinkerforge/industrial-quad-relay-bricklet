8e205300fc79b25a44e7ecd7bcb86ec4efa5ab56
Olaf Lüke <olaf@tinkerforge.com>
2012-12-05 15:58:37 +0100

    Add protocol 2.0 compatability to C# bindings

--- software/examples/csharp/ExampleSimple.cs
+++ software/examples/csharp/ExampleSimple.cs
@@ -8,10 +8,11 @@ class Example
 
 	static void Main() 
 	{
-		IPConnection ipcon = new IPConnection(HOST, PORT); // Create connection to brickd
-		BrickletIndustrialQuadRelay iqr = new BrickletIndustrialQuadRelay(UID); // Create device object
-		ipcon.AddDevice(iqr); // Add device to IP connection
-		// Don't use device before it is added to a connection
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletIndustrialQuadRelay iqr = new BrickletIndustrialQuadRelay(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Turn relays alternating on/off for 10 times with 100 ms delay
 		for(int i = 0; i < 10; i++)
@@ -28,6 +29,5 @@ class Example
 
 		System.Console.WriteLine("Press key to exit");
 		System.Console.ReadKey();
-		ipcon.Destroy();
 	}
 }
