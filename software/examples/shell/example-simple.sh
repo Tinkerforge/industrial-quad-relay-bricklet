980ac348e6f5fa5ced2617a93b1302420dd84b1b
Matthias Bolte <matthias@tinkerforge.com>
2013-08-12 16:49:22 +0200

    shell: Explain magic values

--- software/examples/shell/example-simple.sh
+++ software/examples/shell/example-simple.sh
@@ -4,5 +4,5 @@
 # change to your UID
 uid=XYZ
 
-# turn relay 0, 3 on and relay 1, 2 off
+# turn relay 0, 3 on and relay 1, 2 off: (1 << 0) | (1 << 3) = 9
 tinkerforge call industrial-quad-relay-bricklet $uid set-value 9
