b9b590feca9489b63300667e137264ba2515b7af
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:10:59 +0100

    Add go examples.

---
+++ software/examples/go/example_simple.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"tinkerforge/industrial_quad_relay_bricklet"
+	"tinkerforge/ipconnection"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Industrial Quad Relay Bricklet.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	iqr, _ := industrial_quad_relay_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	// Turn relays alternating on/off 10 times with 100 ms delay
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		iqr.SetValue(1 << 0)
+		time.Sleep(100 * time.Millisecond)
+		iqr.SetValue(1 << 1)
+		time.Sleep(100 * time.Millisecond)
+		iqr.SetValue(1 << 2)
+		time.Sleep(100 * time.Millisecond)
+		iqr.SetValue(1 << 3)
+	}
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
